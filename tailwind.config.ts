e3dcf9c6ffa9695334b16a71c732658e19a233f6
leeozaka <leonardoanicetoneves@hotmail.com>
2024-01-09 01:48:53 -0300

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,21 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'dodger-blue': {
+            '50': '#edfbff',
+            '100': '#d6f5ff',
+            '200': '#b5efff',
+            '300': '#83e8ff',
+            '400': '#48d8ff',
+            '500': '#1ebcff',
+            '600': '#06a0ff',
+            '700': '#0087f5',
+            '800': '#086bc5',
+            '900': '#0d5b9b',
+            '950': '#0e375d',
+        },
+      }
     },
   },
   plugins: [],
