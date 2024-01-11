3d50e530a79dbe896afd422e5d44896d55149222
leeozaka <leonardoanicetoneves@hotmail.com>
2024-01-11 14:46:04 -0300

    Refactor product page and search functionality

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -4,22 +4,28 @@ import { items } from "@/components/items";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 
-const ProductPage = () => {  
+const ProductPage = () => {
   const id = usePathname().replace("/products/", "");
   const product = items.find((item) => item.id === Number(id));
 
   return (
     <div>
       {product ? (
-        <div>
-          <h1>{product.name}</h1>
-          <p>{product.description}</p>
+        <div id="product" className="flex justify-center w-full">
           <Image
             src={product.photo}
             alt={product.name}
             width={208}
             height={208}
           />
+          <div className="flex text-xl flex-col ">
+            <h1>{product.name}</h1>
+            <p>{product.description}</p>
+            <div className="flex flex-row" >
+              <p className="">R${product.price}</p>
+              <p className="text-sm">,00</p>
+            </div>
+          </div>
         </div>
       ) : (
         <p>Product not found</p>
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -7,36 +7,41 @@ const ProductsPage: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState("");
 
   function handleSubmit(searchTerm: string) {
-    for (let i = 0; i < items.length; i++) {
-      if (items[i].name === searchTerm) {
-        return (window.location.href = `/products/${items[i].id}`);
-      }
-    }
-    return (
-      <div>
-        <p>Product not found</p>
-      </div>
+    const actualItem = items.find(
+      (item) => item.name.toLowerCase() === searchTerm.toLowerCase()
     );
+    if (actualItem) {
+      return (window.location.href = `/products/${actualItem.id}`);
+    }
+    document.getElementById("not-found")!.innerHTML = `${searchTerm}&nbsp;`;
+    document.getElementById("not-found-helper")!.innerHTML = `não encontrado`;
   }
 
   return (
     <>
-      <div className="flex justify-center m-0">
-        <input
-          className="w-1/2 p-2 text-center border-2 rounded-lg "
-          type="text"
-          placeholder="Pesquisar"
-          value={searchTerm}
-          onChange={(e) => {
-            setSearchTerm(e.target.value);
-          }}
-          onSubmit={() => handleSubmit(searchTerm)}
-        />{" "}
-        <button
-          className="p-2 rounded-lg border-2 bg-dodger-blue-950 text-white"
-          onClick={() => handleSubmit(searchTerm)}
-        >
-          Buscar        </button>
+      <div className="flex flex-col justify-center m-0">
+        <div id="wrap-input" className="flex justify-center">
+          <input
+            className="w-1/2 p-2 text-center border-2 rounded-lg "
+            type="text"
+            placeholder="Pesquisar"
+            value={searchTerm}
+            onChange={(e) => {
+              setSearchTerm(e.target.value);
+            }}
+          />
+          <button
+            type="submit"
+            className="p-2 rounded-lg border-2 bg-dodger-blue-950 text-white"
+            onClick={() => handleSubmit(searchTerm)}
+          >
+            Buscar
+          </button>
+        </div>
+        <div className="flex justify-center pt-2">
+          <p id="not-found" className="text-red-500 font-bold "></p>
+          <p id="not-found-helper"></p>
+        </div>
       </div>
     </>
   );
